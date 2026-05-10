0efa7b6c8fb9ed5f28394657ae52fd4596b3732d
AltTabber <mikeice414@gmail.com>
2026-05-10 20:58:46 +0200

    remove dialogueManager and add dialogic

--- levels/DialogueStarterExample.gd
+++ levels/DialogueStarterExample.gd
@@ -1,10 +1,8 @@
 extends Usable
 
-@export var dialogueRes: DialogueResource; 
 
 func _ready() -> void:
 	pass
 
 func useObject(): 
-	DialogueManager.show_dialogue_balloon(dialogueRes, "start");
 	pass;
--- levels/mainHall/BookCabinet.gd
+++ levels/mainHall/BookCabinet.gd
@@ -3,4 +3,5 @@ extends Usable
 @export var image: Texture2D
 
 func useObject():
-	MainEventBus.image_layer_show_image.emit(image)
+	Dialogic.start('MainHallCabinetPhoto');
+	get_viewport().set_input_as_handled();

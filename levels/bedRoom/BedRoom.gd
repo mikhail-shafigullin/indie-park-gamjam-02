fafab1c3d7767c31e7d2633f3a94355deda19c66
AltTabber <mikeice414@gmail.com>
2026-05-19 15:10:05 +0200

    added usable in bathroom and added shore animation

--- levels/bedRoom/BedRoom.gd
+++ levels/bedRoom/BedRoom.gd
@@ -4,9 +4,18 @@ extends Level
 @onready var puzzle4Button: Usable = %Solve4;
 @onready var changeScene: Usable = %ChangeScene;
 
+@onready var animatedSprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var animatedSprite2: AnimatedSprite2D = $AnimatedSprite2D2
+@onready var animatedSprite3: AnimatedSprite2D = $AnimatedSprite2D3
+@onready var animatedSprite4: AnimatedSprite2D = $AnimatedSprite2D4
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	player_enters_room.connect(closeDoorFor4thPuzzle);
+	animatedSprite.play()
+	animatedSprite2.play()
+	animatedSprite3.play()
+	animatedSprite4.play()
 	pass # Replace with function body.
 
 
